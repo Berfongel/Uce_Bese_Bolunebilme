e81d4206952117856d39504cdf18c0835d7ebfb8
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 16:11:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi;
+Console.WriteLine("Bir sayı giriniz : ");
+sayi = Convert.ToInt32(Console.ReadLine());
+if (sayi % 3 == 0 && sayi % 5 == 0)
+{
+    Console.WriteLine("Girdiğiniz sayı  3'e ve 5'e bölünüyor. ");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı 3'e ve 5'e tam bölünmüyor");
+}
